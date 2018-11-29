ec82bb9bf0426db47730bb0c55f6f23ae76d0f81
gunberi <enginalamut@gmail.com>
2018-11-29 14:01:33 +0300

    created yapıkredi config file

---
+++ config/yapikredi.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'posnetId' => '',
+	'mid' => '',
+	'tid' => '',
+	'encKey' => '10,10,10,10,10,10,10,10',
+	'returnUrl' => '',
+	'hosts' => [
+		'test' => 'http://setmpos.ykb.com/PosnetWebService/XML',
+		'production' => 'https://posnet.yapikredi.com.tr/PosnetWebService/XML',
+		'test_3d' => 'http://setmpos.ykb.com/3DSWebService/YKBPaymentService',
+		'production_3d' => 'http://posnet.ykb.com/3DSWebService/YKBPaymentService',
+	]
+];
